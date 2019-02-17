23c8efa255a3c32e2b1c36944fe4d5fc50fa9b73
SarveshBajaj <sbsarveshbajaj1998@gmail.com>
2019-02-17 12:04:58 +0530

    Inline Validation Added

--- register.js
+++ register.js
@@ -1,8 +1,22 @@
 var flag;
 
-// if (performance.navigation.type == 1) {
+if (performance.navigation.type == 1) {
+  reset();
+}
 function reset(){
   document.getElementById('form').reset();
+  var email = document.forms["form"]["email"];
+  email.style.background="white";
+  var number = document.forms["form"]["number"];
+  number.style.background="white";
+  var pass1 = document.getElementById("password");
+  var pass2 = document.getElementById("password_confirmation");
+  pass1.style.background="white";
+  pass2.style.background="white";
+  var age = document.forms["form"]["age"];
+  age.style.background="white";
+  var cgpa = document.forms["form"]["cgpa"];
+  cgpa.style.background="white";
 }
 
 function want_admin() {
@@ -29,28 +43,35 @@ function validateEmail(email){
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     // var x = document.forms["form"]["email"];
     if(email.value==""){
+      email.style.background="#e88f91";
       email.setCustomValidity("Please fill out this field.");
       return false;
     }
     if (reg.test(email.value) == false)
     {
+        email.style.background="#e88f91";
         email.setCustomValidity("Invalid Email");
         return false;
     }
     else {
       email.setCustomValidity("");
+      email.style.background="#ccffcc";
     }
     return true;
 }
 
 function validatePassword() {
-  var pass1 = document.getElementById("password").value;
-  var pass2 = document.getElementById("password_confirmation").value;
+  var pass1 = document.getElementById("password");
+  var pass2 = document.getElementById("password_confirmation");
   // alert(pass1 + pass2);
-  if(pass1 != pass2){
+  if(pass1.value != pass2.value || pass1.value==""){
+    pass1.style.background="#e88f91";
+    pass2.style.background="#e88f91";
     document.getElementById("password_confirmation").setCustomValidity("Passwords don't Match");
     return false;
   }
+  pass1.style.background="#ccffcc";
+  pass2.style.background="#ccffcc";
   document.getElementById("password_confirmation").setCustomValidity('');
   return true;
 }
@@ -59,15 +80,18 @@ function validateContact(contact){
     var reg = /^[0-9]{10}$/;
     // var x = document.forms["form"]["number"];
     if(contact.value==""){
+      contact.style.background="#e88f91";
       contact.setCustomValidity("Please fill out this field.");
       return false;
     }
     if (reg.test(contact.value) == false)
     {
+        contact.style.background="#e88f91";
         contact.setCustomValidity("Invalid Contact (10 digits required)");
         return false;
     }
     contact.setCustomValidity("");
+    contact.style.background="#ccffcc";
     return true;
 }
 
@@ -77,14 +101,17 @@ function validateAge(age){
     var reg = /^[0-9]{1,2}$/;
     // var x = document.forms["form"]["age"];
     if(age.value==""){
+      age.style.background="#e88f91";
       age.setCustomValidity("Please fill out this field.");
       return false;
     }
     if (reg.test(age.value) == false || age.value=='0' || age.value=="00")
     {
+        age.style.background="#e88f91";
         age.setCustomValidity("Invalid Age");
         return false;
     }
+    age.style.background="#ccffcc";
     age.setCustomValidity("");
     return true;
 }
@@ -96,19 +123,44 @@ function validateCGPA(cgpa){
     var reg = /^([0-9])+\.+[0-9]$/;
     // var x = document.forms["form"]["cgpa"];
     if(cgpa.value==""){
+      cgpa.style.background="#e88f91";
       cgpa.setCustomValidity("Please fill out this field.");
       return false;
     }
     if (reg.test(cgpa.value) == false)
     {
+        cgpa.style.background="#e88f91";
         cgpa.setCustomValidity("Invalid CGPA, Format Example: 9.0");
         return false;
     }
+    cgpa.style.background="#ccffcc";
     cgpa.setCustomValidity("");
     return true;
 }
 
+function validateName(f_name){
+  if(f_name.value=="")
+  {
+    f_name.style.background="#e88f91";
+    f_name.setCustomValidity("Please fill out this field.");
+    return false;
+  }
+  f_name.style.background="#ccffcc";
+  f_name.setCustomValidity("");
+  return true;
+}
+
 function validateForm(){
+  var name = document.forms["form"]["f_name"];
+  var name_returned = validateName(name);
+  if(name_returned == false)
+    return false;
+
+  var usrnm = document.forms["form"]["usrnm"];
+  var usrnm_returned = validateName(usrnm);
+  if(usrnm_returned == false)
+    return false;
+
   var email = document.forms["form"]["email"];
   var email_returned = validateEmail(email);
   if(email_returned == false){
@@ -134,6 +186,7 @@ function validateForm(){
   var cgpa_returned = validateCGPA(cgpa);
   if(cgpa_returned == false)
     return false;
+
 
   return true;
 }
